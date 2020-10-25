8c52142e4e6cfc5081385c1c3ad1902098f67af0
ftdgomez <dgcodedev@gmail.com>
2020-10-25 18:33:53 +0100

    change Link on PromoCard.js

--- pages/components/PromoCard.js
+++ pages/components/PromoCard.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Link from 'next/link'
 
 const PromoCard = ({subtitle, title, btnText, link, picture}) => {
   return (
     <div className="promocard" style={{backgroundImage: `url(/img/${picture})`}}>
      <h4>{subtitle}</h4> 
      <h2>{title}</h2>
-     <Link href={link}>{btnText}</Link>
+     <a href={link}>{btnText}</a>
     </div>
   )
 }
