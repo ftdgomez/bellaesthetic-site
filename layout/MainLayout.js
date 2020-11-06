69a462c6d1ad845658837e367f1cbafe2df0ddd8
ftdgomez <dgcodedev@gmail.com>
2020-11-06 02:38:33 +0100

    move the script to the Head component

--- layout/MainLayout.js
+++ layout/MainLayout.js
@@ -33,28 +33,28 @@ const MainLayout = ({children, headData}) => {
           <meta name="viewport" content="width=device-width, user-scalable=no"></meta>
           }
           <link rel="stylesheet" href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css"></link>
+          <script dangerouslySetInnerHTML={{__html: `
+              <!-- Load Facebook SDK for JavaScript -->
+              <script>
+                window.fbAsyncInit = function() {
+                  FB.init({
+                    xfbml            : true,
+                    version          : 'v8.0'
+                  });
+                };
+        
+                (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));</script>
+        
+              <!-- Your Chat Plugin code -->
+              `}} />
         </Head>
         <div id="fb-root"></div>
-        <script dangerouslySetInnerHTML={{__html: `
-            <!-- Load Facebook SDK for JavaScript -->
-            <script>
-              window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml            : true,
-                  version          : 'v8.0'
-                });
-              };
-      
-              (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-      
-            <!-- Your Chat Plugin code -->
-            `}} />
             <div class="fb-customerchat"
               attribution={'setup_tool'}
               page_id="182627661817675"
