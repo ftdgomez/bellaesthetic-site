e237ad86248d64d1b2afea419f1dde5fb9e7cf3f
ftdgomez <dgcodedev@gmail.com>
2020-11-06 02:15:23 +0100

    testing danger html for facebook chat

--- layout/MainLayout.js
+++ layout/MainLayout.js
@@ -35,7 +35,31 @@ const MainLayout = ({children, headData}) => {
           <link rel="stylesheet" href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css"></link>
         </Head>
         <Header data={headerData} />
+
+        <div id="fb-root"></div>
+        <script dangerouslySetInnerHTML={{__html: `window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v8.0'
+                });
+              };
+              (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            `}} />
+
+        <div dangerouslySetInnerHTML={{__html: `<div class="fb-customerchat"
+              attribution=setup_tool
+              page_id="182627661817675"
+        theme_color="#e68585">
+            </div>`}}></div>
+
         {children}
+        
         <Footer />
       </>
     )
