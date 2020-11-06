64996713256fd8db970e2c7a57f094daf722b3a4
ftdgomez <dgcodedev@gmail.com>
2020-11-06 02:26:35 +0100

    segunda prueba facebook inject html

--- layout/MainLayout.js
+++ layout/MainLayout.js
@@ -37,26 +37,32 @@ const MainLayout = ({children, headData}) => {
         <Header data={headerData} />
 
         <div id="fb-root"></div>
-        <script dangerouslySetInnerHTML={{__html: `window.fbAsyncInit = function() {
+        <script dangerouslySetInnerHTML={{__html: `
+            <!-- Load Facebook SDK for JavaScript -->
+            <div id="fb-root"></div>
+            <script>
+              window.fbAsyncInit = function() {
                 FB.init({
                   xfbml            : true,
                   version          : 'v8.0'
                 });
               };
+      
               (function(d, s, id) {
               var js, fjs = d.getElementsByTagName(s)[0];
               if (d.getElementById(id)) return;
               js = d.createElement(s); js.id = id;
               js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
               fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-            `}} />
-
-        <div dangerouslySetInnerHTML={{__html: `<div class="fb-customerchat"
+            }(document, 'script', 'facebook-jssdk'));</script>
+      
+            <!-- Your Chat Plugin code -->
+            <div class="fb-customerchat"
               attribution=setup_tool
               page_id="182627661817675"
         theme_color="#e68585">
-            </div>`}}></div>
+            </div>
+            `}} />
 
         {children}
         
