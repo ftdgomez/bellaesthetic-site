dfc09af63a16300402647ad16989675bb320bc8d
ftdgomez <dgcodedev@gmail.com>
2020-11-19 18:27:48 +0100

    fix: variant bg for index services page now is correct

--- components/SinglePageTitle.js
+++ components/SinglePageTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SinglePageTitle = ({data}) => {
   return (
-    <div className="s-title-container bg-primary" style={{backgroundImage: `url(/${data.bg === 'facial' ? 'single-service-top-facial.jpg' : data.bg === 'body' ? 'single-service-top.jpg' : ''})`}}>
+    <div className="s-title-container bg-primary" style={{backgroundImage: `url(/${data.bg === 'facial' ? 'single-service-top-facial.jpg' : data.bg === 'body' ? 'single-service-top-body.jpg' : 'single-service-top.jpg'})`}}>
       <h1 className="text-center">{data.title}</h1> 
     </div>
   )
